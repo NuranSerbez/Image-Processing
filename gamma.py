3c13cd76f8a7cf6818011182ada3c3971fb0a8cb
Nuran Serbez <101194039+NuranSerbez@users.noreply.github.com>
2023-12-23 11:46:19 +0000

    homework 5

---
+++ gamma.py
@@ -0,0 +1,15 @@
+def gamma_correction(gamma):
+    img = cv2.imread(image_path)
+    invGamma = 1 / gamma
+    table = [((i / 255) ** invGamma) * 255 for i in range(256)]
+    table = np.array(table, np.uint8)
+    gammaImg = cv2.LUT(img, table)
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+    plt.title('Orijinal görüntü')
+    plt.subplot(1,2, 2)
+    plt.imshow(gammaImg)
+    plt.title('Gamma değiştirilmiş görüntü')
+    plt.show()
+if __name__ == '__main__':
+    gamma_correction(2.2)
